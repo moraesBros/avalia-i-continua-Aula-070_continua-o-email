00e0c045d96dbfcba62cc90656e7b68de83f2a6f
moraesBros <161986858+moraesBros@users.noreply.github.com>
2025-10-16 04:40:47 -0300

    Update FLASKY_ADMIN email address

--- create_enviroment_variables.sh
+++ create_enviroment_variables.sh
@@ -2,4 +2,4 @@ export FLASK_APP=hello.py
 export FLASK_DEBUG=1
 export MAIL_USERNAME=flaskaulas@zohomail.com
 export MAIL_PASSWORD=$1
-export FLASKY_ADMIN=fabio.teixeira@ifsp.edu.br
+export FLASKY_ADMIN=ednilton.moraes@aluno.ifsp.edu.br
